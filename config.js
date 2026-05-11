112c41f859181273e59d17b22a6324b4a4b8d174
spencer <spencerkirklandd@gmail.com>
2026-05-10 22:13:54 -0700

    fixes

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
 // config.js
 const CONFIG = {
     SUPABASE_URL: 'https://axcigeddgguoqbqueons.supabase.co',
-    SUPABASE_ANON_KEY: 'sb_publishable_Ujl3B5NvCxZzLP_JnZwZZw_csOxpOy4',
-    SUPABASE_SERVICE_KEY: 'sb_secret_uYkGC9pp3kt4ylhZauj56w_bNJMt-_w' // For advanced use
+    SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF4Y2lnZWRkZ2d1b3FicXVlb25zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg0NzM3MTksImV4cCI6MjA5NDA0OTcxOX0.jXhOlubSihxNuebS4OTOekH_P2Io3V0K46gGGc44-Ko'
 };
 
 export default CONFIG;
